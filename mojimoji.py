f647fed1eea0c4821cd13961e5074586c8ebbaac
三嶋哲也 <tmtm.yagi.007@gmail.com>
2017-10-23 03:42:34 +0900

    うごけ

--- mojimoji.py
+++ mojimoji.py
@@ -1,19 +1,45 @@
-from datetime import datetime
-from flask import Flask,jsonify
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return jsonify({
-        "message": "this endpoint is active"
-    })
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    reply_to_line(request.json)
-    return '', 200, {}
 
-if __name__ == '__main__':
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
     app.run()
