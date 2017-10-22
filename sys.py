90339b0909172d9b8027a5f3808cebf256562a34
三嶋哲也 <tmtm.yagi.007@gmail.com>
2017-10-23 02:06:44 +0900

    deploy

--- sys.py
+++ sys.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from MadproBot import app
 
 app = Flask(__name__)
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -15,3 +16,5 @@ def webhook():
     reply_to_line(request.json)
     return '', 200, {}
 
+if __name__ == '__main__':
+    app.run()
