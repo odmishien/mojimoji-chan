046b5109658ed15e76ee0418443a39abc18bd772
三嶋哲也 <tmtm.yagi.007@gmail.com>
2017-10-24 01:47:30 +0900

    testapi

--- mojimoji.py
+++ mojimoji.py
@@ -63,17 +63,7 @@ def handle_img(event):
     message_content = line_bot_api.get_message_content(event.message.id)
     
     import http.client, urllib.request, urllib.parse, urllib.error, base64, json
-
     subscription_key = 'f4f305f4d10548e6abe86b32e98852b0'
-
-    # Replace or verify the region.
-    #
-    # You must use the same region in your REST API call as you used to obtain your subscription keys.
-    # For example, if you obtained your subscription keys from the westus region, replace 
-    # "westcentralus" in the URI below with "westus".
-    #
-    # NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-    # a free trial subscription key, you should not need to change this region.
     uri_base = 'westcentralus.api.cognitive.microsoft.com'
 
     headers = {
@@ -83,16 +73,13 @@ def handle_img(event):
     }
 
     params = urllib.parse.urlencode({
-        # Request parameters. The language setting "unk" means automatically detect the language.
         'language': 'unk',
         'detectOrientation ': 'true',
     })
 
-    # The URL of a JPEG image containing text.
     body = message_content.iter_content()
 
     try:
-        # Execute the REST API call and get the response.
         conn = http.client.HTTPSConnection(uri_base)
         conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
         response = conn.getresponse()
@@ -114,9 +101,8 @@ def handle_img(event):
         print('Error:')
         print(e)
     
-
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=output))
+        event.reply_token,TextSendMessage(text=output))
+
 if __name__ == "__main__":
     app.run()
