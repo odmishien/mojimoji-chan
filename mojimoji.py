3041056fb15413e455e0b07581ef3ff89b83ae46
三嶋哲也 <tmtm.yagi.007@gmail.com>
2017-10-24 00:13:48 +0900

    testapi

--- mojimoji.py
+++ mojimoji.py
@@ -64,7 +64,58 @@ def handle_img(event):
     with tempfile.NamedTemporaryFile() as tf:
         for chunk in message_content.iter_content():
             tf.write(chunk)
-        
+
+    import http.client, urllib.request, urllib.parse, urllib.error, base64, json
+
+    ###############################################
+    #### Update or verify the following values. ###
+    ###############################################
+
+    # Replace the subscription_key string value with your valid subscription key.
+    subscription_key = 'f4f305f4d10548e6abe86b32e98852b0'
+
+    # Replace or verify the region.
+    #
+    # You must use the same region in your REST API call as you used to obtain your subscription keys.
+    # For example, if you obtained your subscription keys from the westus region, replace 
+    # "westcentralus" in the URI below with "westus".
+    #
+    # NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+    # a free trial subscription key, you should not need to change this region.
+    uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+    headers = {
+        # Request headers.
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    params = urllib.parse.urlencode({
+        # Request parameters. The language setting "unk" means automatically detect the language.
+        'language': 'unk',
+        'detectOrientation ': 'true',
+    })
+
+    # The URL of a JPEG image containing text.
+    body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png'}"
+
+    try:
+        # Execute the REST API call and get the response.
+        conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+        conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed = json.loads(data)
+        print ("Response:")
+        print (json.dumps(parsed, sort_keys=True, indent=2))
+        conn.close()
+
+    except Exception as e:
+        print('Error:')
+        print(e)
+
     # line_bot_api.reply_message(
     #     event.reply_token,
     #     ImageSendMessage(
