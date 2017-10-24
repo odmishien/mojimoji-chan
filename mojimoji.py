460002ead92a35edfb091f79e9484af25af9cf2e
三嶋哲也 <tmtm.yagi.007@gmail.com>
2017-10-25 01:17:20 +0900

    うごけ

--- mojimoji.py
+++ mojimoji.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage
+    MessageEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate
 )
 
 app = Flask(__name__)
