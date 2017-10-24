2e2613e12a5ff19ae3190e45e39cde813ed37214
三嶋哲也 <tmtm.yagi.007@gmail.com>
2017-10-25 02:02:28 +0900

    手書きとそうでないのとで分けた

--- mojimoji.py
+++ mojimoji.py
@@ -133,12 +133,12 @@ def handle_img(event):
         body = message_content.iter_content()
         params = {'handwriting' : 'true'}
         try:
-        conn = http.client.HTTPSConnection(uri_base)
-        conn.request('POST','/vision/v1.0/RecognizeText%s'%params,body,headers)
-        response = conn.getresponse()
-        data = response.read()
-        print(data)
-        conn.close()
+            conn = http.client.HTTPSConnection(uri_base)
+            conn.request('POST','/vision/v1.0/RecognizeText%s'%params,body,headers)
+            response = conn.getresponse()
+            data = response.read()
+            print(data)
+            conn.close()
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
