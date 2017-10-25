0e87ce103083d2eb3c32f0408676df6252d5850c
三嶋哲也 <tmtm.yagi.007@gmail.com>
2017-10-25 11:07:11 +0900

    手書きとそうでないのとで分けた

--- mojimoji.py
+++ mojimoji.py
@@ -8,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent,PostbackEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate,MessageTemplateAction,PostbackTemplateAction
+    MessageEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage
 )
 
 app = Flask(__name__)
@@ -17,8 +17,6 @@ line_bot_api = LineBotApi('HvlRdc8yEbGXkq/V9cRJ6/+yRgJuZGNFEzx7I7p/TUovvMhuVSwLW
 handler = WebhookHandler('23172aefb6f71f1e78f643b171b6c389')
 
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-
-moji_format = ""
 
 def make_static_tmp_dir():
     try:
@@ -31,50 +29,25 @@ def make_static_tmp_dir():
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
+    # get request body as text
     body = request.get_data(as_text=True)
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return ''
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("textmessageハンドラが起動しました")
     line_bot_api.reply_message(
         event.reply_token,
-        [TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-        thumbnail_image_url='https://example.com/image.jpg',
-        title='もじもじちゃんです',
-        text='読み取りたい文字を選んでください！',
-        actions=[
-            PostbackTemplateAction(
-                label='手書き',
-                data='our'
-            ),
-            PostbackTemplateAction(
-                label='手書き以外(印刷物など)',
-                data='ocr'
-            )
-        ]
-    )
-)])
-    return ""
-
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    post = event.postback
-    global moji_format
-    moji_format = post.data
-    line_bot_api.reply_message(
-        event.reply_token,[TextSendMessage(text="画像を送ってくださいね！")])
-
-    return ""
+        TextSendMessage(text="画像を送ってクレメンス"))
 
 @handler.add(MessageEvent,message=ImageMessage)
 def handle_img(event):
@@ -83,7 +56,7 @@ def handle_img(event):
     else:
         sorry_text='画像以外は送れません、ごめんなさい!'
         line_bot_api.reply_message(
-            event.reply_token, [TextSendMessage(text=sorry_text)])
+            event.reply_token, TextSendMessage(text=sorry_text))
         return
 
     message_content = line_bot_api.get_message_content(event.message.id)
@@ -93,64 +66,42 @@ def handle_img(event):
     uri_base = 'westcentralus.api.cognitive.microsoft.com'
 
     headers = {
+        # Request headers.
         'Content-Type': 'application/octet-stream',
         'Ocp-Apim-Subscription-Key': subscription_key,
     }
-    
-    global moji_format
-    #手書き文字以外の解析
-    if moji_format == "ocr":
-        params = urllib.parse.urlencode({
-            'language': 'unk',
-            'detectOrientation ': 'true',
-        })
 
-        body = message_content.iter_content()
+    params = urllib.parse.urlencode({
+        'language': 'unk',
+        'detectOrientation ': 'true',
+    })
 
-        try:
-            conn = http.client.HTTPSConnection(uri_base)
-            conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
-            response = conn.getresponse()
-            result = response.read()
+    body = message_content.iter_content()
 
-            output = ""
-            parsed = json.loads(result)
-            if parsed ['regions'] == []:
-                output = "文字は見当たりません。。。"
-            else:
-                for txt_lines in parsed['regions']:
-                    for txt_words in txt_lines['lines']:            
-                        for txt_word in txt_words['words']:
-                            if parsed['language'] == 'ja':
-                                output += txt_word['text']
-                            else:
-                                output += txt_word['text'] + ' '
-                        output += '\n'
+    try:
+        conn = http.client.HTTPSConnection(uri_base)
+        conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+
+        output = ""
+        parsed = json.loads(data)
+        print(parsed['regions'])
+        if parsed ['regions'] == []:
+            output = "文字は見当たりません。。。"
+        else:
+            for txt_lines in parsed['regions']:
+                for txt_words in txt_lines['lines']:            
+                    for txt_word in txt_words['words']:
+                        if parsed['language'] == 'ja':
+                            output += txt_word['text']
+                        else:
+                            output += txt_word['text'] + ' '
                     output += '\n'
-        except Exception as e:
-            print('Error:')
-            print(e)
-            print("ocrだよ")
-
-    #手書き文字の解析
-    else:
-        body = message_content.iter_content()
-        params = urllib.parse.urlencode({'handwriting' : 'true',})
-        try:
-            conn = http.client.HTTPSConnection(uri_base)
-            conn.request('POST','/vision/v1.0/recognizeText?%s' % params, body, headers)
-            response = conn.getresponse()
-            data = response.read()
-            print(data)
-            output = ""
-            parsed = json.loads(data) 
-            print("これはパースです")
-            print(parsed)
-            conn.close()
-        except Exception as e:
-            print("Error:")
-            print(e)
-            print("ourだよ")
+                output += '\n'
+    except Exception as e:
+        print('Error:')
+        print(e)
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=output))
 if __name__ == "__main__":
